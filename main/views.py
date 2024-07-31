ea3cde1fb2faf1e9366ac063ea4e078c745af5ad
ncgordeev <ncgordeev@gmail.com>
2024-07-31 17:39:05 +0300

    added base template

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+    extra_context = {'title': 'Главная страница'}
